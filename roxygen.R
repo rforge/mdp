72fb6107585050549ebf69ba4823fcbb8104c82f
relund <relund@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-08 12:32:21 +0000

    Version 1.1: Updated doc and added vignette. Tried to run R CMD CHECK.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mdp@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- roxygen.R
+++ roxygen.R
@@ -3,10 +3,7 @@ roxygenize("pkg","pkg",copy.package=FALSE) # unlink.target=TRUE)
 
 # hack to remove the files we don't want to have Rd files for
 # since documented elsewhere
-unlink(c("pkg/man/actionIdxDf.Rd","pkg/man/actionIdxMat.Rd",
-	"pkg/man/actionInfo.Rd","pkg/man/actionWeightMat.Rd",
-	"pkg/man/stateIdxDf.Rd","pkg/man/transProbMat.Rd",
-	"pkg/man/deleteHMDP.Rd","pkg/man/checkWDurIdx.Rd",
+unlink(c("pkg/man/deleteHMDP.Rd","pkg/man/checkWDurIdx.Rd",
 	"pkg/man/checkWIdx.Rd","pkg/man/firstLib.Rd"
 	))
 
